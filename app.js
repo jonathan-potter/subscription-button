14166c72ba21c820ae1e41cc1881c48ec6ff0ca2
Jonathan Potter <jonathan.potter.business@gmail.com>
2014-10-27 21:01:46 -0700

    simple state machine working

--- app.js
+++ app.js
@@ -1,77 +1,68 @@
 ;(function () {
-    var emailSubscriptionButton, eventWrapper, subscribeButtonState, subscribeForm;
+    var emailSubscriptionButton, EmailSubscriptionButton
 
-    subscribeButtonState = "unsubscribed";
-    subscribeForm = $(".subscribe-form");
-
-    // subscribeForm.click(function (event) {
-    //     var submitButton, targetElement;
-    //
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //
-    //     targetElement = event.currentTarget;
-    //
-    //     // if (subscribeForm.hasClass("unsubscribed")) {
-    //    //      subscribeForm.removeClass("unsubscribed");
-    //    //      subscribeForm.addClass("subscribing");
-    //    //  } else if (subscribeForm.hasClass("subscribing")) {
-    //    //      subscribeForm.removeClass("subscribing");
-    //    //      subscribeForm.addClass("subscribed");
-    //    //  }
-    //
-    //    submitButton = $(".subscribe-form:focus input ~ div, .subscribe-form *:focus ~ div");
-    //
-    //    if (submitButton === targetElement) {
-    //        subscribeForm.addClass("subscribed");
-    //    }
-    // });
-
-    stateCascade = {
-        "unsubscribed": "subscribing",
-         "subscribing": "subscribed"
-    }
-
-    EmailSubscriptionButton = function () { this.initialize() };
+    EmailSubscriptionButton = function (state) { this.initialize(state) };
 
     EmailSubscriptionButton.prototype = {
         initialize: function (state) {
-               this.state = state || "unsubscribed";
+            this.state = state || "unsubscribed";
 
-                this.form = $(".subscribe-form");
-               this.glyph = form.find("aside");
-               this.input = form.find("input");
-              this.button = form.find("div");
-            this.elements = [
-                this.glyph,
-                this.input,
-                this.buttom
-            ];
-        },
-        advanceState: function () {
-            switch (this.state) {
-                case "unsubscribed":
-                    this.state = "subscribing"
-                    break;
-                case "subscribing":
-                    this.state = "subscribing"
-                    break;
+            this.form = $(".subscribe-form");
+            this.elements = {
+                 form: this.form,
+                glyph: this.form.find("aside"),
+                input: this.form.find("input"),
+               button: this.form.find("div")
+            }
+            this.states = {
+                "unsubscribed": {
+                        nextState: "subscribing",
+                    bindingTarget: this.elements.form
+                },
+                "subscribing": {
+                        nextState: "subscribed",
+                    bindingTarget: this.elements.button
+                }
             }
 
-            updateUI();
+            this.updateUI();
+        },
+        advanceState: function () {
+            this.state = this.stateInformation().nextState;
+
+            this.updateUI();
+        },
+        bindHandlers: function () {
+            var self, stateInformation;
+
+            self = this;
+            stateInformation = this.stateInformation();
+
+            if (stateInformation && stateInformation.bindingTarget) {
+                stateInformation.bindingTarget.click(function () {
+                    self.advanceState.call(self);
+                });
+            }
+        },
+        initializeDomState: function () {
+            for (state in this.states) {
+                this.form.removeClass(state);
+            }
+
+            for (element in this.elements) {
+                this.elements[element].off("click");
+            }
+        },
+        stateInformation: function () {
+            return this.states[this.state];
         },
         updateUI: function () {
             this.initializeDomState();
-
-            this.form.addClass(state);
-        },
-        initializeDomState: function () {
-            for (state in stateCascade) {
-                this.form.removeClass(state);
-            }
+            this.form.addClass(this.state);
+            this.bindHandlers();
         }
     };
 
-    esb = new EmailSubscriptionButton();
+    emailSubscriptionButton = new EmailSubscriptionButton();
 
 })();
