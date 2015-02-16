ce5cd29c1aec5d76b3b871cb382912da1bde10e2
Jonathan Potter <jonathan.potter.business@gmail.com>
2015-02-16 13:32:36 -0800

    moved files, moved handlers into non-anonymous methods

--- scripts/app.js
+++ scripts/app.js
@@ -88,6 +88,12 @@
                         target: this.elements.input,
                         state: "unsubscribed"
                     }
+                },
+                "subscription-submitted": {
+                    click: {
+                        target: this.elements.form,
+                        state: "unsubscribed"
+                    }
                 }
             }
 
@@ -105,79 +111,12 @@
             events = this.stateInformation();
 
            if (events) {
-               events.click && events.click.target.click(function (event) {
-                   event.stopPropagation();
-
-                   self.setState(events.click.state);
-                   self.elements.input.focus();
-               });
-               events.textAdded && events.textAdded.target.keyup(function (event) {
-                   event.stopPropagation();
-
-                   var text;
-
-                   text = events.textAdded.target.val();
-
-                   if (text.length > 0) {
-                       self.setState(events.textAdded.state);
-                   }
-               });
-               events.lossOfFocus && events.lossOfFocus.target.focusout(function (event) {
-                   event.stopPropagation();
-
-                   var element, targetInElements;
-
-                   targetInElements = false;
-                   for (elementIndex in self.elements) {
-                       element = self.elements[elementIndex];
-                       if (element[0] === event.relatedTarget) {
-                           targetInElements = true;
-                       }
-                   }
-
-                   if (!targetInElements) {
-                       self.setState(events.lossOfFocus.state);
-                   }
-               });
-               events.regexPassed && events.regexPassed.target.keyup(function (event) {
-                   event.stopPropagation();
-
-                   var emailMatch, emailRegexp, text;
-
-                   emailRegexp = /[a-z]+@[a-z]+\.[a-z]+/;
-                   text = events.regexPassed.target.val();
-
-                   emailMatch = text.match(emailRegexp);
-
-                   if (emailMatch) {
-                       self.setState(events.regexPassed.state);
-                   }
-               });
-               events.regexFailed && events.regexFailed.target.keyup(function (event) {
-                   event.stopPropagation();
-
-                   var emailMatch, emailRegexp, text;
-
-                   emailRegexp = /[a-z]+@[a-z]+\.[a-z]+/;
-                   text = events.regexFailed.target.val();
-
-                   emailMatch = text.match(emailRegexp);
-
-                   if (!emailMatch) {
-                       self.setState(events.regexFailed.state);
-                   }
-               });
-               events.allTextRemoved && events.allTextRemoved.target.keydown(function (event) {
-                   event.stopPropagation();
-
-                   var text;
-
-                   text = events.regexPassed.target.val();
-
-                   if (text.length === 0) {
-                       self.setState(events.allTextRemoved.state);
-                   }
-               });
+               events.click && events.click.target.click(this.respondToClick.bind(this));
+               events.textAdded && events.textAdded.target.keyup(this.respondToKeyUp.bind(this));
+               events.lossOfFocus && events.lossOfFocus.target.focusout(this.respondToLossOfFocus.bind(this));
+               events.regexPassed && events.regexPassed.target.keyup(this.respondToRegexPassed.bind(this));
+               events.regexFailed && events.regexFailed.target.keyup(this.respondToRegexFailed.bind(this));
+               events.allTextRemoved && events.allTextRemoved.target.keydown(this.allTextRemoved.bind(this));
            }
         },
         initializeDomState: function () {
@@ -198,6 +137,81 @@
             this.initializeDomState();
             this.form.addClass(this.state);
             this.bindHandlers();
+        },
+
+        /** EVENT HANDLERS **/
+        respondToClick: function (event) {
+           event.stopPropagation();
+
+           this.setState(events.click.state);
+           this.elements.input.focus();
+        },
+        respondToKeyUp: function (event) {
+           event.stopPropagation();
+
+           var text;
+
+           text = events.textAdded.target.val();
+
+           if (text.length > 0) {
+               this.setState(events.textAdded.state);
+           }
+        },
+        respondToLossOfFocus: function (event) {
+           event.stopPropagation();
+
+           var element, elementIndex, targetInElements;
+
+           targetInElements = false;
+           for (elementIndex in this.elements) {
+               element = this.elements[elementIndex];
+               if (element[0] === event.relatedTarget) {
+                   targetInElements = true;
+               }
+           }
+
+           if (!targetInElements) {
+               this.setState(events.lossOfFocus.state);
+           }
+        },
+        respondToRegexPassed: function (event) {
+            event.stopPropagation();
+
+            var emailMatch, emailRegexp, text;
+
+            emailRegexp = /[a-z]+@[a-z]+\.[a-z]+/;
+            text = events.regexPassed.target.val();
+
+            emailMatch = text.match(emailRegexp);
+
+            if (emailMatch) {
+                this.setState(events.regexPassed.state);
+            }
+        },
+        respondToRegexFailed: function (event) {
+            event.stopPropagation();
+
+            var emailMatch, emailRegexp, text;
+
+            emailRegexp = /[a-z]+@[a-z]+\.[a-z]+/;
+            text = events.regexFailed.target.val();
+
+            emailMatch = text.match(emailRegexp);
+
+            if (!emailMatch) {
+                this.setState(events.regexFailed.state);
+            }
+        },
+        allTextRemoved: function (event) {
+            event.stopPropagation();
+
+            var text;
+
+            text = events.regexPassed.target.val();
+
+            if (text.length === 0) {
+                this.setState(events.allTextRemoved.state);
+            }
         }
     };
 
